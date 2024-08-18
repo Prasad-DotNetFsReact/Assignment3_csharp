9f8c233a028ffdf9b0d06b2a2ca4deae3df650bc
Prasad Poul <prasadpoul.2024.com>
2024-08-18 11:26:58 +0530

    third assignment

---
+++ Assignment31/Assignment31/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Question5
+    {
+        public static void Main(string[] args) {
+            int[] array = { 2, 2, 7, 8, 8, 6, 9, 9, 9, 1, 5 };
+
+            int duplicateCount = CountDuplicateElements(array);
+
+            Console.WriteLine($"total number of duplicate:{duplicateCount}");
+        }
+     
+        static int CountDuplicateElements(int[] array)
+        { Array.Sort(array);
+            int duplicateCount = 0;
+
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] == array[i + 1])
+                {
+                    duplicateCount++;
+
+                    while (i < array.Length - 1 && array[i] == array[i + 1])
+                    {
+                        i++;
+                    }
+                }
+                
+            }
+
+            return duplicateCount;
+        }
+     
+}
+}
---
+++ Assignment33/Assignment33/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Question3
+    {
+        public static void Main(string[] args)
+        {
+            const double rateUpto199 = 1.20;
+            const double rate200To399 = 1.50;
+            const double rate400To599 = 1.00;
+            const double rate600AndAbove = 2.00;
+            const double minimumBill = 100.00;
+            const double surchargeThreshhold = 400.00;
+            const double surchargePercentage = 0.15;
+
+
+
+
+            Console.WriteLine("Welcome on app");
+            
+
+            Console.WriteLine("Enter your CustomId:");
+            int cId=byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Name:");
+            string cName= Console.ReadLine();
+
+            Console.WriteLine("Enter your used units:");
+            double unit = byte.Parse(Console.ReadLine());
+
+            double initialBill = 0;
+            if (unit <= 199)
+            {
+                initialBill=unit*rateUpto199;
+            }
+            else if(unit>=200 && unit<=399){
+                initialBill*=unit*rate200To399;
+
+            }
+            else if(unit>=400 && unit <= 599)
+            {
+                initialBill = unit * rate400To599;
+            }
+            else {
+                initialBill = initialBill * rate600AndAbove;
+            }
+            double finalFill = initialBill;
+            if (initialBill > surchargeThreshhold)
+            {
+                double surcharge = initialBill * surchargePercentage;
+                finalFill = finalFill + surcharge;
+            }
+
+            if (finalFill < minimumBill)
+            {
+                finalFill = minimumBill;
+            }
+                    Console.WriteLine($"Total bill: {finalFill:F2}");
+                  
+            
+        }
+    }
+}
